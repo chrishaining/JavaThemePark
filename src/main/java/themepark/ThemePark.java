f4d55cae3bf072bd1714183685efd12cd5fce15f
Chris Haining <chrishaining@yahoo.co.uk>
2019-10-15 21:04:18 +0100

    adds themepark

---
+++ src/main/java/themepark/ThemePark.java
@@ -0,0 +1,25 @@
+package themepark;
+
+import attractions;
+import stalls;
+
+public class ThemePark {
+
+    private String name;
+
+    public ThemePark(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
+
+
+
+
+
+
+
